6767b284809161302813ea3ca8a58360c5585ee2
Eric Roberson <fcs2014eroberson@gmail.com>
2024-04-04 11:25:20 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = (): JSX.Element => {
+  type Pokemon = {
+    name:string,
+    url: string,
+  }
+
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then((data) => {
+        setPokemon(data.results);
+      });
+  }, []);
+
+  console.log(pokemon[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemon.map(
+          (data) => (
+              <div>
+                {data.name.toString()}
+                {data.url.toString()}
+              </div>
+          ))}
     </div>
   );
 }
